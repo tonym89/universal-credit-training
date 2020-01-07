c8dfbe3bbaaa21e9360de46c9905fbc4c6e1b906
Anthony McShane <a89th@hotmail.com>
2020-01-07 19:32:10 +0000

    service worker enabled.  state added to journal to handle user inputs

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -129,6 +129,9 @@ function checkValidServiceWorker(swUrl, config) {
 export function unregister() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.ready.then(registration => {
+      console.log(
+        'Wit u daein'
+      );
       registration.unregister();
     });
   }
