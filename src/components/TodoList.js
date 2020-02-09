6b8bf9b4202b88898b93cecf7aff16c1b5b787b9
Anthony McShane <a89th@hotmail.com>
2020-02-09 12:23:32 +0000

    feat: todo list and home components added

---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TodoList(){
+
+    return (
+        <div>
+         <p className="govuk-body">Your to-do list is empty.</p>
+        </div>
+    )
+}
+
+export default TodoList;
